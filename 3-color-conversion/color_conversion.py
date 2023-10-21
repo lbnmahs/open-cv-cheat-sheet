01ee044a5a5899fa2e3fee38ed8c1e833e46730c
LbnMahs <labanmahs2002@gmail.com>
2023-10-21 16:08:30 +0300

    commenting

--- 3-color-conversion/color_conversion.py
+++ 3-color-conversion/color_conversion.py
@@ -6,6 +6,7 @@ img = cv2.imread(os.path.join('.', 'data', 'bmw_1.jpg'))
 # Play around with the cv2.COLOR_ method to see the different types of color schemes
 img_converted = cv2.cvtColor(img, cv2.COLOR_RGB2HSV)
 
+# Displaying the images
 cv2.imshow('Original Image', img)
 cv2.imshow('Color Converted Image', img_converted)
 cv2.waitKey(0)
