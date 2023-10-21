0e9ab326b1c8e8da078a62786fe6fbf162117094
LbnMahs <labanmahs2002@gmail.com>
2023-10-21 16:07:59 +0300

    color conversion, done

---
+++ 3-color-conversion/color_conversion.py
@@ -0,0 +1,11 @@
+import os
+import cv2
+
+img = cv2.imread(os.path.join('.', 'data', 'bmw_1.jpg'))
+# This example is converting the image to HSV color scheme
+# Play around with the cv2.COLOR_ method to see the different types of color schemes
+img_converted = cv2.cvtColor(img, cv2.COLOR_RGB2HSV)
+
+cv2.imshow('Original Image', img)
+cv2.imshow('Color Converted Image', img_converted)
+cv2.waitKey(0)
